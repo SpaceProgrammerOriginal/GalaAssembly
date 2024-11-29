4e30b729e70cd8348f98df91e50c810858a0dd70
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2024-11-29 22:44:41 +0100

    Created base folder structure

---
+++ G1611/compiler.py
@@ -0,0 +1,5 @@
+"""
+Compiler of the official Galaicum16v1_1 arquitecture
+
+Supported CPUs: Gala I
+"""
---
+++ G1611/text_processing.py
@@ -0,0 +1,4 @@
+def tokenize_code(code : str) -> list[list[str]]:
+
+    comment_split = code.split("#") #separar per comentaris
+    
